5b307b6ed71ff1235b597c438189e283e9462452
Kelsy Vaughn <angelstate@gmail.com>
2020-04-22 10:15:03 -0700

    setup environment

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,12 @@
+import React from 'react';
+import ReactDOM from'react-dom';
+
+const App = () => {
+  return (
+    <div>Hello World!</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
 
---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+        {
+          test: /\.css$/,
+          loader: 'style-loader',
+        },
+        {
+          test: /\.css$/,
+          loader: 'css-loader',
+          query: {
+            modules: true,
+          },
+        },
+          {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
